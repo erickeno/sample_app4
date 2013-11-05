0308f566c56691e073c06b962c28535e6e5faaf6
erick bett <erickeno@yahoo.com>
2013-11-05 17:20:46 -0500

    Added more refined and DRY tests in rspec

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,13 @@
+
+# require 'spec_helper'
+
+# describe  ApplicationHelper do
+
+# 	describe "full_title" do 
+# 		it "should include the page title" do 
+# 			expect(full_title())
+# 		end
+# 	end
+
+# end
+
--- spec/requests/statick_pages_spec.rb
+++ spec/requests/statick_pages_spec.rb
@@ -4,17 +4,24 @@ require 'spec_helper'
 
 describe "StatickPages" do
 
-	let(:base_title) {"Ruby on Rails Tutorial Sample App"}
+	#let(:base_title) {"Ruby on Rails Tutorial Sample App"}
 
 	subject { page }
+
+	#shared examples for all the pages
+	shared_examples_for "all statick pages" do 
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
 
 	#This is testing form the home page
 
 	describe "Home page" do
 		before {visit root_path}
+		let (:heading) { 'Sample App' }
+		let (:page_title) { '' }
 
-		it { should have_content('Sample App') }
-		it { should have_title(full_title('')) }
+		it_should_behave_like "all statick pages"
 		it { should_not have_title(' | Home') }
 	end
 
@@ -22,30 +29,60 @@ describe "StatickPages" do
 
 	describe "Help page" do
 		before {visit help_path}
+		let (:heading) { 'Help' }
+		let (:page_title) { 'Help' }
 
-		it { should have_content('Help') }
-		it { should have_title(full_title('Help')) }
+		it_should_behave_like "all statick pages"
 	end
 
 	#This is testing for the About us page.
 
 	describe "About page" do
 		before { visit about_path}
+		let (:heading) {'About Us' }
+		let (:page_title) { 'About Us' }
 
-		it { should have_content('About Us') }
-		it { should have_title(full_title('About Us')) }
+		it_should_behave_like "all statick pages"
 	end
 
 	#creating and testing the contact page
 	describe "Contact page" do
 		before { visit contact_path}
+		let (:heading) { 'Contact Us' }
+		let (:page_title) { 'Contact Us' }
 
-		it { should have_content('Contact Us') }
-		it { should have_title(full_title('Contact Us')) }
+		it_should_behave_like "all statick pages"
 	end
 
-	
+	#testing  that we have the right links on the layout
+	it "should have the right links in the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About Us'))
+
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+
+		click_link "Contact" 
+		expect(page).to have_title(full_title('Contact Us'))
+
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign Up'))
+
+		click_link "sample app"
+		expect(page).not_to have_title(full_title(' | Home'))
+	end
 end
+
+
+
+
+
+
+
+
+
 
 
 
